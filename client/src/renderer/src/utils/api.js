4f305efda670b6f61cce0dbe759073119bb350a4
ehgk4245 <dohakim.dev@gmail.com>
2026-03-12 14:05:54 +0900

    feat: aiFetch 요청 엑세스 토큰 만료 시 토큰 재발급 로직 호출

--- client/src/renderer/src/utils/api.js
+++ client/src/renderer/src/utils/api.js
@@ -8,7 +8,23 @@ export async function aiFetch(url, options = {}) {
     ...options.headers,
     Authorization: `Bearer ${token}`
   }
-  return fetch(`${AI_BASE_URL}${url}`, { ...options, headers })
+
+  let res = await fetch(`${AI_BASE_URL}${url}`, { ...options, headers })
+
+  if (res.status === 401) {
+    const ok = await refreshTokens()
+    if (!ok) {
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+      window.location.hash = '/'
+      throw new Error('세션이 만료되었습니다. 다시 로그인해 주세요.')
+    }
+    const newToken = localStorage.getItem('accessToken')
+    headers.Authorization = `Bearer ${newToken}`
+    res = await fetch(`${AI_BASE_URL}${url}`, { ...options, headers })
+  }
+
+  return res
 }
 
 async function refreshTokens() {
